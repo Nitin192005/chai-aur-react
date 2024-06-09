fdfb51d1effbd0a6dba3b5d47a419f607945c7c8
NITIN PANDEY <144930015+Nitin192005@users.noreply.github.com>
2024-06-09 14:48:18 +0530

    Update App.jsx

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,42 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react' // Importing useState hook from React
+import './App.css' // Importing styles
 
 function App() {
+  // Declaring state variable 'counter' and its setter 'setCounter' using this we can make changes in the ui of the website
+  const [counter, setCounter] = useState(0)
 
-  const [counter, setCounter]  = useState(15)
-
-  //let counter = 15
-
-  const addValue = () => {
-    //counter = counter + 1
-    setCounter(prevCounter => prevCounter + 1)
-    setCounter(prevCounter => prevCounter + 1 )
-    setCounter(prevCounter => prevCounter + 1)
-    setCounter(prevCounter => prevCounter + 1)
-    
+  // Function to increment counter
+  const increment = () => {
+    setCounter(counter + 1)
   }
 
-  const removeValue = () => {
+  // Function to decrement counter
+  const decrement = () => {
     setCounter(counter - 1)
   }
-  
+
   return (
     <>
-      <h1>Chai aur react</h1>
-      <h2>Counter value: {counter}</h2>
-
-      <button
-      onClick={addValue}
-      >Add value {counter}</button> 
-      <br />
-      <button
-      onClick={removeValue}
-      >remove value {counter}</button>
-      <p>footer: {counter}</p>
+      <h1 className='p-4 bg-pink-600 text-3xl'>Counter App</h1>
+      <p className='text-xl'>{counter}</p>
+      <button onClick={increment} className='m-2 p-2 bg-green-500 text-white'>
+      Increment
+      </button>
+      
+      <button onClick={decrement} className='m-2 p-2 bg-red-500 text-white'>
+        Decrement
+      </button>
     </>
   )
 }
 
-export default App
+export default App // Exporting App component as default
+
